03dbac63fa73a982a82d2cf107a565d877fc8736
Kantapon Dechjarernsi <59431606+KantaponDech@users.noreply.github.com>
2021-05-17 02:14:38 +0700

    Update create_table.sh

--- 02_sql/create_table.sh
+++ 02_sql/create_table.sh
@@ -2,3 +2,4 @@ bash authorize.sh
 
 MYSQLIP=$(gcloud sql instances describe chess --format="value(ipAddresses.ipAddress)")
 mysql --host=$MYSQLIP --user=root --password --verbose < create_table.sql
+*
